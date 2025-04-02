4a520cd00c6a4c84c0912c71c2ff84dabde9e1fa
Matthew Johnston <warmwaffles@gmail.com>
2025-04-02 00:34:03 -0500

    Updates to the latest version of elixir
    * Drops Elixir 1.14 support.
    * Drops OTP 24 support.
    * Adds OTP 27 support
    * Adds Elixir 1.17 support.
    * Adds Elixir 1.18 support.

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule FileStore.MixProject do
     [
       app: :file_store,
       version: @version,
-      elixir: "~> 1.14",
+      elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
